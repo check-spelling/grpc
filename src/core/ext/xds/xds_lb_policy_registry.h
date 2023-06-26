8b570a6fef650382354f0c1c3a45380a75e37aef
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-06-26 09:02:31 -0400

    spelling: maintains
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/core/ext/xds/xds_lb_policy_registry.h
+++ src/core/ext/xds/xds_lb_policy_registry.h
@@ -31,7 +31,7 @@
 
 namespace grpc_core {
 
-// A registry that maintans a set of converters that are able to map xDS
+// A registry that maintains a set of converters that are able to map xDS
 // loadbalancing policy configurations to gRPC's JSON format.
 class XdsLbPolicyRegistry {
  public:
