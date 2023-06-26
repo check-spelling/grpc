89933ebde469514f7cf15ac25093a05d2e046c9c
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-06-26 09:07:14 -0400

    spelling: unknown
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- third_party/upb/upb/util/compare.c
+++ third_party/upb/upb/util/compare.c
@@ -215,7 +215,7 @@ static upb_UnknownFields* upb_UnknownFields_Build(upb_UnknownField_Context* ctx,
   return fields;
 }
 
-// Compares two sorted upb_UnknwonFields structures for equality.
+// Compares two sorted upb_UnknownFields structures for equality.
 static bool upb_UnknownFields_IsEqual(const upb_UnknownFields* uf1,
                                       const upb_UnknownFields* uf2) {
   if (uf1->size != uf2->size) return false;
