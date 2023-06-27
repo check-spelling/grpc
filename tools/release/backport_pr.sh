b47989d6b6e2186b7bb9371dc16190230788cee5
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-06-26 22:00:26 -0400

    spelling: cherry-picking
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- tools/release/backport_pr.sh
+++ tools/release/backport_pr.sh
@@ -31,7 +31,7 @@ USAGE: $0 PR_ID GITHUB_USER BACKPORT_BRANCHES REVIEWERS [-c PER_BACKPORT_COMMAND
    GITHUB_USER: Your GitHub username.
    BACKPORT_BRANCHES: A space-separated list of branches to which the source PR will be backported.
    REVIEWERS: A comma-separated list of users to add as both reviewer and assignee.
-   PER_BACKPORT_COMMAND : An optional command to run after cherrypicking the PR to the target branch.
+   PER_BACKPORT_COMMAND : An optional command to run after cherry-picking the PR to the target branch.
      If you use this option, ensure your working directory is clean, as "git add -A" will be used to
      incorporate any generated files. Try running "git clean -xdff" beforehand.
 
